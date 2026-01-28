7a895738ce089fc9ae3d9914f7b96e5502e1f69f
Ahmed-Osama-Sovereign-OS <elhajosama78@gmail.com>
2026-01-28 05:56:56 -0500

    Update build_iso.sh

--- build_iso.sh
+++ build_iso.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Building Ahmed Osama Universe ISO..."
-mkdir -p build/iso_root
-cp main_core.py build/iso_root/
-cp installer_logo.png build/iso_root/
-# أمر توليد الـ ISO (يتطلب أدوات genisoimage)
-genisoimage -o Ahmed_Osama_OS.iso -R -J build/iso_root
-echo "The Monster ISO is Ready!"
+echo "Building Ahmed Osama Universe OS ISO..."
+# كود تحويل الملفات إلى ISO
+genisoimage -o Ahmed_Osama_OS.iso -R -J .
+echo "The Monster is Ready for Action!"
